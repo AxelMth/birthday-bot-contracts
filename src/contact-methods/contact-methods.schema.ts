c11668b6510ca9c62e1b1761531d3e2e4f60399d
Axel Mathieu-Le Gall <aujoulator@hotmail.fr>
2025-08-24 14:19:41 +0200

    Update applicationMetadata field in contactMethodSchema to use nullish instead of nullable for improved flexibility

--- src/contact-methods/contact-methods.schema.ts
+++ src/contact-methods/contact-methods.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const contactMethodSchema = z.object({
   id: z.number(),
   applicationName: z.string(),
-  applicationMetadata: z.record(z.string(), z.union([z.string(), z.number(), z.boolean(), z.null()])).nullable(),
+  applicationMetadata: z.record(z.string(), z.union([z.string(), z.number(), z.boolean(), z.null()])).nullish(),
 });
 
 export const getContactMethodsResponseSchema = z.object({
